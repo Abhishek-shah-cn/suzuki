ec4390ca88cd9dca868becc6cc6f83889f5cf4a7
Abhishekh Shah <abhishekhshah@Abhishekhs-MacBook-Air.local>
2025-05-20 01:32:24 +0530

    changes in styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",  // for older Next.js versions
-    "./app/**/*.{js,ts,jsx,tsx}",    // for app directory support (Next.js 13+)
-    "./components/**/*.{js,ts,jsx,tsx}", // include your components folder
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
